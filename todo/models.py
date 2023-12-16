193a04f8c1d36a4f67ebe9c85cb7619a389a60f3
Angelos Grigorakis <angelosgreg13@gmail.com>
2023-12-16 15:31:27 +0100

    models debug

--- todo/models.py
+++ todo/models.py
@@ -5,3 +5,5 @@ from django.utils import timezone
 class ToDoItem(models.Model):
     text = models.CharField(max_length=100)
     due_date = models.DateField(default=timezone.now)
+def __str__(self):
+    return f"{self.text}: due {self.due_date}"
