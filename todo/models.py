409a44334fbf08acc27d52f037d806d6ea0ba4bb
Angelos Grigorakis <angelosgreg13@gmail.com>
2023-12-19 12:22:45 +0100

    debug

--- todo/models.py
+++ todo/models.py
@@ -7,3 +7,6 @@ class ToDoItem(models.Model):
     due_date = models.DateField(default=timezone.now)
 def __str__(self):
     return f"{self.text}: due {self.due_date}"
+
+def get_queryset(self):
+    return ToDoItem.objects.filter(due_date__gte=date.today())
